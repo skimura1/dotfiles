8a5c7108397d6239509765dd7b238fa63c792a78
Skyler Kimura <49466672+ryvami@users.noreply.github.com>
2023-09-04 23:29:10 -1000

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,31 +13,33 @@ end
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+-- For example, changig the color scheme:
+config.color_scheme = 'Dracula'
+config.default_domain = "WSL:Debian"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.inactive_pane_hsb = {
   saturation = 0.8,
   brightness = 0.7,
 }
 config.window_decorations = "RESIZE"
-config.window_frame = {
-  font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
-}
 config.use_dead_keys = false
 config.scrollback_lines = 5000
+config.font_size = 13
 config.line_height = 1.2
+window_frame = {
+ font = wezterm.font { family = 'Noto Sans', weight = 'Regular' },
+}
 config.hide_tab_bar_if_only_one_tab = true
 config.disable_default_key_bindings = true
 config.leader = { key = "Space", mods = "CTRL" }
 config.keys = {
   { key = "l", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(1) },
   { key = "h", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(-1) },
-  { key = "h", mods = "CTRL",       action = act.ActivatePaneDirection("Left") },
-  { key = "l", mods = "CTRL",       action = act.ActivatePaneDirection("Right") },
-  { key = "k", mods = "CTRL",       action = act.ActivatePaneDirection("Up") },
-  { key = "j", mods = "CTRL",       action = act.ActivatePaneDirection("Down") },
-  { key = "t", mods = "LEADER",     action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "h", mods = "CTRL|SHIFT",       action = act.ActivatePaneDirection("Left") },
+  { key = "l", mods = "CTRL|SHIFT",       action = act.ActivatePaneDirection("Right") },
+  { key = "k", mods = "CTRL|SHIFT",       action = act.ActivatePaneDirection("Up") },
+  { key = "j", mods = "CTRL|SHIFT",       action = act.ActivatePaneDirection("Down") },
+  { key = "c", mods = "LEADER",     action = act.SpawnTab("CurrentPaneDomain") },
   { key = "f", mods = "LEADER",     action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
   { key = "d", mods = "LEADER",     action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
   { key = "x", mods = "LEADER",     action = act.CloseCurrentPane({ confirm = false }) },
@@ -45,6 +47,8 @@ config.keys = {
   { key = "[", mods = "LEADER",     action = act.ActivateCopyMode },
   { key = "r", mods = "LEADER",     action = act.ReloadConfiguration },
   { key = "C", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
+  { key = "v", mods = "SHIFT|CTRL", action = act.PasteFrom("PrimarySelection")},
+  { key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard")},
   {
     key = "k",
     mods = "CTRL|ALT",
@@ -55,6 +59,14 @@ config.keys = {
   },
   { key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
 }
+
+for i = 1, 8 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1),
+  })
+end
 
 -- and finally, return the configuration to wezterm
 return config
