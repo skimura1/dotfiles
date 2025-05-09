dc6668aa5ff134fa1c0b3bf5389c975d6c85c97e
Skyler Kimura <skyler8@hawaii.edu>
2025-05-09 12:06:16 -1000

    added asdf to fish terminal

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,6 +21,7 @@ alias ll="exa -l -g"
 alias llt="exa -1 --tree"
 alias vi="nvim"
 alias py="python3"
+alias todo="nvim ~/Documents/notes/todo.md"
 starship init fish | source
 zoxide init fish | source
 
@@ -41,3 +42,17 @@ else
     end
 end
 # <<< conda initialize <<<
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+	set _asdf_shims "$HOME/.asdf/shims"
+else    
+	set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+	set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+fish_add_path /Users/skyler/.spicetify
